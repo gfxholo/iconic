5d9d2eb33c79cf1336b36aa3939290e7784db85a
unxok <anderluke707@gmail.com>
2025-12-23 21:37:24 +1100

    fix: remove hardcoded default property icons
    Based on PR #175.
    
    Co-authored-by: Holo <gfx.holo@proton.me>

--- src/IconicPlugin.ts
+++ src/IconicPlugin.ts
@@ -1129,18 +1129,10 @@ export default class IconicPlugin extends Plugin {
 	 */
 	private definePropertyItem(propBase: any, unloading?: boolean): PropertyItem {
 		const propIcon = this.settings.propertyIcons[propBase.name] ?? {};
-		let iconDefault;
-		switch (propBase.widget ?? propBase.type) { // Pre-1.9.0 compatible
-			case 'text': iconDefault = 'lucide-text'; break;
-			case 'multitext': iconDefault = 'lucide-list'; break;
-			case 'number': iconDefault = 'lucide-binary'; break;
-			case 'checkbox': iconDefault = 'lucide-check-square'; break;
-			case 'date': iconDefault = 'lucide-calendar'; break;
-			case 'datetime': iconDefault = 'lucide-clock'; break;
-			case 'aliases': iconDefault = 'lucide-forward'; break;
-			case 'tags': iconDefault = 'lucide-tags'; break;
-			default: iconDefault = 'lucide-file-question'; break;
-		}
+		// @ts-expect-error (Private API)
+		const widget = this.app.metadataTypeManager?.getWidget?.(propBase.widget ?? propBase.type);
+		const iconDefault = widget?.icon ?? 'lucide-file-question';
+
 		return {
 			id: propBase.name,
 			name: propBase.name,
